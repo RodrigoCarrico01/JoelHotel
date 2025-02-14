36c890b696730ea0306184f4490017c562b8eb52
Rodrigo Martins <mr.rodrigocarrico@gmail.com>
2025-02-14 10:41:21 +0000

    Image Field added into usermodel

--- backend/models/User.js
+++ backend/models/User.js
@@ -26,6 +26,10 @@ const UserSchema = new mongoose.Schema(
             enum: ["admin", "utilizador"],
             default: "utilizador",
         },
+        profilePic: {
+            type: String,
+            default: "https://example.com/default-profile.png",
+        },
     },
     { timestamps: true }
 );
