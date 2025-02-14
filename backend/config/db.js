e699604fc006aef6f357d3f4ba11146e2715216a
Rodrigo Martins <mr.rodrigocarrico@gmail.com>
2025-02-14 10:20:28 +0000

    Restart; Trying to apply better Git behavior

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,10 @@ require("dotenv").config();
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB conectado!");
+        console.log("🔥 MongoDB conectado com sucesso!");
     } catch (error) {
-        console.error("Erro ao conectar ao MongoDB:", error);
-        process.exit(1);
+        console.error("❌ Erro ao conectar ao MongoDB:", error);
+        process.exit(1); // Força a saída do processo em caso de falha
     }
 };
 
