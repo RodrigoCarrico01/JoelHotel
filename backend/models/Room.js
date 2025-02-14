0ffbf1a89ca6bcf7756d801224bbc2ba8c1c2884
Rodrigo Martins <mr.rodrigocarrico@gmail.com>
2025-02-14 11:20:08 +0000

    Room model

---
+++ backend/models/Room.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const RoomSchema = new mongoose.Schema(
+    {
+        nome: {
+            type: String,
+            required: [true, "O nome do quarto é obrigatório"],
+        },
+        descricao: {
+            type: String,
+            required: [true, "A descrição do quarto é obrigatória"],
+        },
+        imagem: {
+            type: String, // URL da imagem
+            required: [true, "A imagem do quarto é obrigatória"],
+        },
+        numeroQuarto: {
+            type: String,
+            required: [true, "O número do quarto é obrigatório"],
+            unique: true,
+        },
+        precoPorNoite: {
+            type: Number,
+            required: [true, "O preço por noite é obrigatório"],
+        },
+        status: {
+            type: String,
+            enum: ["disponível", "ocupado"],
+            default: "disponível",
+        },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Room", RoomSchema);
